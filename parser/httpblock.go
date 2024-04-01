46fd7b0af8116c35a488aa287b6abbc2d1d9747f
Alexander Tikhonov <alex.vl.tihon@gmail.com>
2024-04-01 23:26:51 +0700

    Add http block

---
+++ parser/httpblock.go
@@ -0,0 +1,25 @@
+package parser
+
+type HttpBlock struct {
+	Block
+}
+
+func (b *HttpBlock) FindBlocks(blockName string) []Block {
+	return b.parser.FindBlocks(blockName)
+}
+
+func (b *HttpBlock) FindServerBlocks() []ServerBlock {
+	return b.parser.FindServerBlocks()
+}
+
+func (b *HttpBlock) FindServerBlocksByServerName(serverName string) []ServerBlock {
+	return b.parser.FindServerBlocksByServerName(serverName)
+}
+
+func (b *HttpBlock) FindUpstreamBlocks() []UpstreamBlock {
+	return b.parser.FindUpstreamBlocks()
+}
+
+func (b *HttpBlock) FindUpstreamBlocksByName(upstreamName string) []UpstreamBlock {
+	return b.parser.FindUpstreamBlocksByName(upstreamName)
+}
